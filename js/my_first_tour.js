b94b2d41b6972fd7ded847b8d1fd519afd6aa572
MICHELLE CHOI <mchoi@college.harvard.edu:git clone https://github.com/mchoi4194/2015lab1.gitgit config --global user.email mchoi@college.harvard.edu>
2015-12-07 22:35:38 -0500

    hopscotch

--- js/my_first_tour.js
+++ js/my_first_tour.js
@@ -2,60 +2,37 @@
     var tour = {
       id: "hello-hopscotch",
       steps: [
+        // {
+        //   title: "User representation",
+        //   content: "The demographic of casual vs. registered users is represented here based on the selections previoiusly made. ",
+        //   target: "pieVis",
+        //   placement: "right"
+        // },
         {
           title: "Welcome",
           content: "Toggle these sliders to set values for these variables.",
-          target: "alltext",
-          placement: "bottom"
-        },
-        {
-          title: "Toggle",
-          content: "Toggle between weekend or weekday departure data, to update all visualizations.",
-          target: "weekend",
-          placement: "bottom"
-        },
-        {
-          title: "Brush over time",
-          content: "Use the brush to select the time frame you would like the visualize the number of arrivals for a specific station. When no station is selected, the data represents aggregated departues from all stations.",
-          target: "countVis",
+          target: "fundingslider",
           placement: "right"
         },
         {
-          title: "User representation",
-          content: "The demographic of casual vs. registered users is represented here based on the selections previoiusly made. ",
+          title: "Demographic Info",
+          content: "Alter demographic information and watch the pie charts update.",
+          target: "slider",
+          placement: "bottom"
+        },
+        {
+          title: "Hover",
+          content: "Hover over the legends to highlight your selection.",
           target: "pieVis",
           placement: "right"
         },
         {
-          title: "Age and gender representation",
-          content: "The demographic of age and gender is represented here based on the selections previoiusly made. ",
-          target: "userVis",
+          title: "Infer Results",
+          content: "See the inferred score based on the variables you selected.",
+          target: "surveyscorevalue",
           placement: "right"
         },
-        {
-          title: "Capacity",
-          content: "When you select a station, you can see the capacity of that station",
-          target: "capacitybar",
-          placement: "bottom"
-        },
-        {
-          title: "Toggle",
-          content: "Toggle between the size of the circles on the map representing either capacity or departures",
-          target: "capacitybar",
-          placement: "bottom"
-        },
-        {
-          title: "Hover Feature",
-          content: "Every legend has a hover feature in which elements coresponding to the legend are highlighted so it is more clear to the user",
-          target: "legendhere",
-          placement: "left"
-        },
-         {
-          title: "Station visualization",
-          content: "Each hubway station updates based on selections previously made. The size of the circle of each station represents either the number of departures of that station or the capacity of the station, and the color of the station represents the percent full of bikes at that station.",
-          target: "mapVis",
-          placement: "left"
-        }
+        
       ]
     };
     hopscotch.startTour(tour);
